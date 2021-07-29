fef56071c1722875cce7f6a8991975d953f927f7
candela <brunetcandela@gmail.com>
2021-07-29 20:52:55 -0300

    Creamos el navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import './navBar.css'
+
+function NavBar(){
+    return <div className="navbar">
+        
+ <ul>
+ <li><p className="brandName">Butterfly Store</p></li>
+  <li><a href="default.html">Inicio</a></li>
+  <li><a href="news.asp">Combos</a></li>
+  <li><a href="contact.asp">Productos</a></li>
+  <li><a href="about.asp">Contacto</a></li>
+</ul>
+    
+    </div>
+}
+
+export default NavBar;
